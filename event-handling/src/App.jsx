eff80f11e9861a68253311b53376b3d6aa9e4337
Kartikay Sharma <kartikaysharma6840@gmail.com>
2023-08-17 16:45:22 +0530

    deleted unused folders

--- event-handling/src/App.jsx
+++ event-handling/src/App.jsx
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+
+    const text="Click Me!";
+    const color="#8e44ad";
+    const [bg, setBg] = useState(color);
+    const [name, setName] = useState(text);
+
+
+    const bgChange = () =>{
+
+        let newCol = "green";
+        setBg(newCol);
+
+    }
+
+    const expName = () =>{
+
+        let newName="Ouch";
+        setName(newName);
+
+    }
+
+    const dbClick = () => {
+
+        let newCol = "yellow";
+        setBg(newCol);
+
+        let dbName="Bas!";
+        setName(dbName);
+
+    }
+
     return (
-        <h1>Hello World</h1>
+        <>
+            <div style={{backgroundColor:bg}}>
+                <button
+                onClick={()=>{
+                    bgChange();
+                    expName();
+                }}
+                onDoubleClick={dbClick}>
+                    {name}
+                </button>
+            </div>
+        </>
     )
 }
 
