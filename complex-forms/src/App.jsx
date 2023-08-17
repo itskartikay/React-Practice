b170c124b3f4f17ebd6fc2e0940871cfa604b8cf
Kartikay Sharma <kartikaysharma6840@gmail.com>
2023-08-18 00:06:46 +0530

    added css and made it simple

--- complex-forms/src/App.jsx
+++ complex-forms/src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
+import "./App.css"
 
 const App = () => {
 
   const [fullName, setFullName] = useState({
     fname:"",
     sname:"",
-    email:""
+    email:"",
+    phone:""
   });
 
   const onSubmit = (event) =>{
@@ -17,76 +19,103 @@ const App = () => {
     console.log(event.target.value)
     console.log(event.target.name)
 
-    const name = event.target.name
-    const value = event.target.value
+    // const name = event.target.name
+    // const value = event.target.value
+
+    const {name, value} = event.target
 
     setFullName((preValue)=>{
-      if(name==='fname'){
-        return{
-          fname: value,
-          sname: preValue.sname,
-          email: preValue.sname
-
-        }
-      }else if(name==='sname'){
-        return{
-          fname: preValue.fname,
-          sname: value,
-          email: preValue.sname
-        }
-      }
-      else if(name==='email'){
-        return{
-          fname: preValue.fname,
-          sname: preValue.sname,
-          email: value
-        }
+      return{
+        ...preValue,
+        [name] : value
       }
     })
+
+    // setFullName((preValue)=>{
+
+
+    //   if(name==='fname'){
+    //     return{
+    //       fname: value,
+    //       sname: preValue.sname,
+    //       email: preValue.sname
+
+    //     }
+    //   }else if(name==='sname'){
+    //     return{
+    //       fname: preValue.fname,
+    //       sname: value,
+    //       email: preValue.sname
+    //     }
+    //   }
+    //   else if(name==='email'){
+    //     return{
+    //       fname: preValue.fname,
+    //       sname: preValue.sname,
+    //       email: value
+    //     }
+    //   }
+    // }
+    //)
 
   }
 
   return(
     <>
-      <form onSubmit={onSubmit}>
-          <h1>
-              Hello {fullName.fname} {fullName.sname}
-          </h1>
-          <p>
-            Email - {fullName.email}
-          </p>
+      <div>
+        <form onSubmit={onSubmit}>
+            <h1>
+                Hello {fullName.fname} {fullName.sname}
+            </h1>
+            <p>
+              Email ID - {fullName.email}
+              <br/>
+              <br/>
+              Phone Number - {fullName.phone}
+            </p>
 
-          <input
-            type="text"
-            placeholder="First Name"
-            name="fname"
-            value={fullName.fname}
-            onChange={inputEvent}>
-          </input>
+            <input
+              type="text"
+              placeholder="First Name"
+              name="fname"
+              value={fullName.fname}
+              onChange={inputEvent}>
+            </input>
 
-          <br/>
+            <br/>
 
-          <input
-            type="text"
-            placeholder="Second Name"
-            name="sname"
-            value={fullName.sname}
-            onChange={inputEvent}>
-          </input>
+            <input
+              type="text"
+              placeholder="Second Name"
+              name="sname"
+              value={fullName.sname}
+              onChange={inputEvent}>
+            </input>
 
-          <br/>
+            <br/>
 
-          <input
-            type="text"
-            placeholder="Email"
-            name="email"
-            value={fullName.email}
-            onChange={inputEvent}>
-          </input>
+            <input
+              type="text"
+              placeholder="Email Address"
+              name="email"
+              value={fullName.email}
+              onChange={inputEvent}>
+            </input>
 
-          <br/>
-          <button onClick={onSubmit}>Click Me</button>
-      </form>
+            <br/>
+
+            <input
+              type="number"
+              placeholder="Phone Number"
+              name="phone"
+              value={fullName.phone}
+              onChange={inputEvent}>
+            </input>
+
+            <br/>
+            <button onClick={onSubmit}>Click Me</button>
+        </form>
+      </div>
     </>
   )
 }
