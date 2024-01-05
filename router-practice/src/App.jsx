88a82292ab5e4d68bee2b66cfe7a829f808d8c1e
Kartikay Sharma <kartikaysharma6840@gmail.com>
2024-01-05 21:51:35 +0530

    addition of router

--- router-practice/src/App.jsx
+++ router-practice/src/App.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
+    <>
+      <p>
+        OLA!
+      </p>
+    </>
   );
 }
 
