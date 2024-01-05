29b328bb7464c5636f0711852ac0ce027e4bfcd2
Kartikay Sharma <kartikaysharma6840@gmail.com>
2024-01-05 22:08:27 +0530

    router added and working

--- router-practice/src/App.jsx
+++ router-practice/src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
 
-function App() {
+const App = () => {
   return (
     <>
-      <p>
-        OLA!
-      </p>
+    <Routes>
+      <Route path="/" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+    </Routes>
+
+    {/* <About/>
+    <Contact/> */}
+
     </>
   );
 }
